b7f72195db0e4793e85d6684bb1db1a195f58fc6
Anthony Le Goas <anthony.legoas.partner@decathlon.com>
2021-10-11 15:56:07 +0200

    ✨ add counter-js page

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -18,6 +18,9 @@
 			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
 			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
 			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
+			<li class:active={$page.path === '/counter-js'}>
+				<a sveltekit:prefetch href="/counter-js">Counter JS</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
---
+++ src/routes/counter-js.svelte
@@ -0,0 +1,44 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Counter JS</title>
+	<!-- He, we import the counter compiled to JavaScript with Rollup. -->
+	<script defer src="../../package-js/counter.js"></script>
+	<link rel="stylesheet" href="../../package-js/counter.css" />
+</svelte:head>
+
+<div class="content">
+	<h1>Here, the counter is compiled to JavaScript with Rollup before to be used.</h1>
+
+	<p>
+		You need to run the <code>package:js</code> script to compile the counter in JavaScript.
+	</p>
+
+	<!-- The counter will take place in the following div. -->
+	<div id="svelte-counter" />
+</div>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
